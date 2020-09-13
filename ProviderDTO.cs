c31acc5188072a68b0221002566bdd95e1bbe7a4
stefanoprivileggi <55325653+stefanoprivileggi@users.noreply.github.com>
2020-09-13 13:51:45 -0300

    Add files via upload
    Buen dia, 
    ¿Cuales son los pasos para entregar la actividad?, Es la primera vez que utilizo GitHub y estoy medio perdido.
    Realice toda la actividad, comparto los archivos. 
    
    Saludos. 
    Stefano Privileggi

---
+++ ProviderDTO.cs
@@ -0,0 +1,12 @@
+namespace Stock.Api.DTOs
+{
+    public class ProviderDTO
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ ProviderSearchDTO.cs
@@ -0,0 +1,9 @@
+namespace Stock.Api.DTOs
+{
+    public class ProviderSearchDTO
+    {
+        public string Name { get; set; }
+
+        public string Condition { get; set; }
+    }
+}
